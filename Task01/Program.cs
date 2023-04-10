612a8dd257630873eb6241b616ff32bd6b76aa68
kolwaii <kolwaii.kz0202@mail.ru>
2023-04-11 00:20:05 +0600

    Удаление доп.условие в Task01 на 13 строке

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -10,7 +10,7 @@ Console.WriteLine("Введите первое целое число");
 int numberA = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе целое число");
 int numberB = Convert.ToInt32(Console.ReadLine());
-if ((numberA == numberB * numberB)|(numberB == numberA * numberA)){
+if (numberA == numberB * numberB){
     Console.WriteLine("yes");
 }
 else
