2ee6fa6539f324da9b09542d23b6c335f1c0bce2
kolwaii <kolwaii.kz0202@mail.ru>
2023-04-11 01:10:00 +0600

    added Task05, Task007

---
+++ Task05/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает
+// одно число (N), а на выходе показывает все целые
+// числа в промежутке от -N до N.
+// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
+// 2 -> " -2, -1, 0, 1, 2"
+
+
+Console.WriteLine("Введите целое число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int count = -numberA;
+while (count <= numberA)
+{
+    Console.Write(count + " ");
+    count++;
+}
