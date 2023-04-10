7ff6cf8448701f4ae36e113c681f26183a62f48a
kolwaii <kolwaii.kz0202@mail.ru>
2023-04-11 00:05:09 +0600

    Выполнил задания 001,003 и добавил шаблон для домашнего задания Task002

---
+++ Task03/Program.cs
@@ -0,0 +1,40 @@
+﻿// Напишите программу, которая будет выдавать
+// название дня недели по заданному номеру.
+// 3 -> Среда
+// 5 -> Пятница
+
+Console.WriteLine("Введите номер дня недели - от 1 до 7");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+if (number == 2)
+{
+    Console.WriteLine("Вторник");
+}
+if (number == 3)
+{
+    Console.WriteLine("Среда");
+}
+if (number == 4)
+{
+    Console.WriteLine("Четверг");
+}
+if (number == 5)
+{
+    Console.WriteLine("Пятница");
+}
+if (number == 6)
+{
+    Console.WriteLine("суббота");
+}
+if (number == 7)
+{
+    Console.WriteLine("Воскресенье");
+}
+else if ((number < 1)|(number > 7))
+{
+    Console.WriteLine("Вы ввели неправильный номер!");
+}
