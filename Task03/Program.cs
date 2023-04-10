1b822e4c9391e4822e84cac5effadda92fc553c1
kolwaii <kolwaii.kz0202@mail.ru>
2023-04-11 00:24:33 +0600

    Задача Task003

--- Task03/Program.cs
+++ Task03/Program.cs
@@ -10,31 +10,31 @@ if (number == 1)
 {
     Console.WriteLine("Понедельник");
 }
-if (number == 2)
+else if (number == 2)
 {
     Console.WriteLine("Вторник");
 }
-if (number == 3)
+else if (number == 3)
 {
     Console.WriteLine("Среда");
 }
-if (number == 4)
+else if (number == 4)
 {
     Console.WriteLine("Четверг");
 }
-if (number == 5)
+else if (number == 5)
 {
     Console.WriteLine("Пятница");
 }
-if (number == 6)
+else if (number == 6)
 {
     Console.WriteLine("суббота");
 }
-if (number == 7)
+else if (number == 7)
 {
     Console.WriteLine("Воскресенье");
 }
-else if ((number < 1)|(number > 7))
+else 
 {
     Console.WriteLine("Вы ввели неправильный номер!");
 }
